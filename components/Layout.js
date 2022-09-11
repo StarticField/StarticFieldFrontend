91b019f89ca9034b81b650f116d620b57b858a38
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2022-09-11 22:51:25 +0530

    sub final

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,11 @@
 import { Box } from "./Box.js";
 import Nav from "./Nav.js";
 import styles from "../styles/layout.module.css"
-import Footer from "./Footer";
 
 export const Layout = ({ children }) => (
   <Box>
     <div className={styles.navTopSpace}></div>
     <Nav></Nav>
     {children}
-    <Footer></Footer>
   </Box>
 );
