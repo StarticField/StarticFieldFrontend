55df20f2e5fc713d80d663636133cd89474b8227
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2022-09-19 15:01:33 +0530

    recent events done ✅

--- components/RecentEvents.js
+++ components/RecentEvents.js
@@ -32,9 +32,9 @@ export default class RecentEvents extends Component {
             dots: true,
             infinite: true,
             slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
+            // slidesToScroll: 1,
+            // autoplay: true,
+            // autoplaySpeed: 2000,
             pauseOnHover: true,
             nextArrow: <SampleNextArrow />,
             prevArrow: <SamplePrevArrow />,
@@ -55,7 +55,12 @@ export default class RecentEvents extends Component {
                 <Slider {...settings}>
                     <div>
                         <div className={styles.card}>
-                            <div className={styles.cardImage}></div>
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                        src="https://www.youtube-nocookie.com/embed/-tJRl-yEeV4"
+                                        title="YouTube video player" frameBorder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                        allowFullScreen>
+                            </iframe>
                             <div className={styles.cardText}>
                                 <span className={styles.date}>Shamik Guha</span>
                                 <h2>CEO & Co-Founder of Altor</h2>
@@ -68,7 +73,32 @@ export default class RecentEvents extends Component {
                     </div>
                     <div>
                         <div className={styles.card}>
-                            <div className={styles.cardImage}></div>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/iqLK-Dvk_EE"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
+                            <div className={styles.cardText}>
+                                <span className={styles.date}>Martynas Fedotovas</span>
+                                <h2>Space Start Ups To The Moon</h2>
+                                <p>
+                                    Shamik Guha - CEO & Co-Founder of Altor discussed his journey in his
+                                    startup, which was influenced by the death of a friend.....
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.card}>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/YyCR7gK7zS"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
                             <div className={styles.cardText}>
                                 <span className={styles.date}>Shamik Guha</span>
                                 <h2>CEO & Co-Founder of Altor</h2>
@@ -81,7 +111,13 @@ export default class RecentEvents extends Component {
                     </div>
                     <div>
                         <div className={styles.card}>
-                            <div className={styles.cardImage}></div>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/y3ZQaoT_N_4"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
                             <div className={styles.cardText}>
                                 <span className={styles.date}>Shamik Guha</span>
                                 <h2>CEO & Co-Founder of Altor</h2>
@@ -94,7 +130,13 @@ export default class RecentEvents extends Component {
                     </div>
                     <div>
                         <div className={styles.card}>
-                            <div className={styles.cardImage}></div>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/fM0KsSCNjvk"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
                             <div className={styles.cardText}>
                                 <span className={styles.date}>Shamik Guha</span>
                                 <h2>CEO & Co-Founder of Altor</h2>
@@ -107,7 +149,13 @@ export default class RecentEvents extends Component {
                     </div>
                     <div>
                         <div className={styles.card}>
-                            <div className={styles.cardImage}></div>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/zVnP8d6Erm4"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
                             <div className={styles.cardText}>
                                 <span className={styles.date}>Shamik Guha</span>
                                 <h2>CEO & Co-Founder of Altor</h2>
@@ -120,7 +168,32 @@ export default class RecentEvents extends Component {
                     </div>
                     <div>
                         <div className={styles.card}>
-                            <div className={styles.cardImage}></div>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/70rt1tSmUYc"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
+                            <div className={styles.cardText}>
+                                <span className={styles.date}>Shamik Guha</span>
+                                <h2>CEO & Co-Founder of Altor</h2>
+                                <p>
+                                    Shamik Guha - CEO & Co-Founder of Altor discussed his journey in his
+                                    startup, which was influenced by the death of a friend.....
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.card}>
+
+                            <iframe className={styles.cardImage} width="100%" height="100%"
+                                    src="https://www.youtube.com/embed/B_Amore8LNs"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
                             <div className={styles.cardText}>
                                 <span className={styles.date}>Shamik Guha</span>
                                 <h2>CEO & Co-Founder of Altor</h2>
