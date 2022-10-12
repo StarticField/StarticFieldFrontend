e46bfff381a3b349b1b8ee06985c85674585e752
Ashsenior <ashutoshjunior@gmail.com>
2022-10-12 22:25:26 +0530

    F

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -110,17 +110,7 @@ const Wrapper = styled(Box)(
 `
 );
 
-const token = (typeof window !== "undefined")?localStorage.getItem("access_token"):"none";
-const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
-    timeout: 5000,
-    headers: {
-        'Authorization': "JWT " + token,
-        'Content-Type': 'application/json',
-        'accept': 'application/json'
-    }
-});
-
+import axiosInstance from "../components/axiosAPI";
 const SignUp = () => {
 
   const [username,setUsername] = useState("");
